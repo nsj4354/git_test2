213ca82f8c096c20d3c551658c2bbaf289d4f8ff
NaSeJong1 <nsj4354@gmail.com>
2025-04-25 16:47:14 +0900

    test

---
+++ demo/src/main/java/com/example/AnonyMousInnerMain_05.java
@@ -0,0 +1,20 @@
+package com.example;
+
+class OutterRun{
+    Runnable getRunnable(){
+        int num =100;
+
+        return new Runnable(){
+
+            public void run()
+            {
+
+            }
+        };
+    }
+    
+}
+
+public class AnonyMousInnerMain_05 {
+
+}
